de53345e7e4b7007d972d4ad986a6c6affc46d7e
Josue Luque <jluqueherbas@gmail.com>
2023-12-28 16:31:45 -0300

    Fix db

--- src/db/db.py
+++ src/db/db.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/restapidb")
+engine = create_engine("mysql+pymysql://root:@localhost:3306/restapidb") # Es un objeto de sqlalchemy.create_engine que se utiliza para conectarse a una base de datos
 
-meta = MetaData()
+meta = MetaData() # Es un objeto de sqlalchemy.MetaData que se utiliza para almacenar información sobre la estructura de la base de datos, como las tablas y sus columnas.
 
-conn = engine.connect
+conn = engine.connect() 
 
 
 
--- src/models/userModel.py
+++ src/models/userModel.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Table, Column
-from sqlalchemy.sql.sqltypes import Integer, Integer, String
+from sqlalchemy.sql.sqltypes import Integer, String
 from src.db.db import meta, engine
 
-users = Table('users', meta, 
-              Column('id', Integer, primary_key=True), 
-              Column('name', String(255)),
-              Column('email', String(255)),
-              Column('password', String(255)))
+users = Table("users", meta, 
+              Column("id", Integer, primary_key=True, autoincrement=True), 
+              Column("name", String(255)),
+              Column("email", String(255)),
+              Column("password", String(255)),)
 
-meta.create_all(engine)
+meta.create_all(engine) # Creará todas las tablas definidas en el objeto meta en la base de datos especificada por el objeto engine.
