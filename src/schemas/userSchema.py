35d0ce86644e62f3a4e9466d68dcd3b0c5126b85
Josue Luque <jluqueherbas@gmail.com>
2023-12-28 16:34:29 -0300

    Fix userSchema

--- src/schemas/userSchema.py
+++ src/schemas/userSchema.py
@@ -2,9 +2,10 @@ from typing import Optional
 from pydantic import BaseModel
 
 class User(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     name: str
     email: str
     password: str
+
 
 
