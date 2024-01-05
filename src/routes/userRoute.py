a09407ae3538346f8582173fa39c2f5dd86f22b4
Josue Luque <jluqueherbas@gmail.com>
2024-01-05 12:47:08 -0300

    Added put, delete and changes in name routes

--- src/routes/userRoute.py
+++ src/routes/userRoute.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from src.schemas.userSchema import User
 from src.services import userService
+from starlette.status import HTTP_204_NO_CONTENT
 
 
 user = APIRouter()
@@ -8,22 +9,36 @@ user = APIRouter()
 #TODO Agregar Exceptions y Status code
 #TODO Seperar funciones en user service y agregar docstrings
 
-@user.get("/users")
+@user.get("/user/users", response_model=list[User], tags=["user"])
 def get_users():
     return userService.all_users()
 
-@user.post("/user")
+@user.post("/user/createUser", response_model=User, tags=["user"])
 def create_user(user: User):
-    return userService.new_user(user)
+    return userService.add_new_user(user)
 
-@user.get("/users/{id}")
+@user.get("/user/getUser/{userId}", tags=["user"])
 def get_user(id: str):
-    return userService.find_by_id(id)
+    result = userService.find_by_id(id)
+    if (result):
+        return result
+    else: 
+        return f"No se encontro el usuario con id: {id}"
 
-@user.put("/users/{id}")
-def update_user(user: User, id: str):
-    return "user edited"
+@user.put("/user/editUser/{userId}", response_model=User, tags=["user"])
+def update_user(id: str,user: User):
+    exis_user = userService.find_by_id(id)
+    if (exis_user):
+        return userService.update_by_id(id,user)
+    else:
+        return "Failed to update user"
 
-@user.delete("/users/{id}")
+
+@user.delete("/user/removeUser/{userId}", tags=["user"])
 def delete_user(id: str):
-    return "user deleted"
+    exis_user = userService.find_by_id(id)
+    if (exis_user):
+        userService.delete_by_id(id)
+        return "Successfully deleted"
+    else:
+        return "Failed to delete user"
