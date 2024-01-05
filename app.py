4c769c0080e730d228457d7444a1ca4a6ce66c84
Josue Luque <jluqueherbas@gmail.com>
2024-01-05 12:45:31 -0300

    added entry point

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from src.routes.userRoute import user
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(
+    title="System Users API",
+    description="REST API using Python, FastAPI, MySQL and SQLAlchemy ",
+    version="0.1.0"
+)
 
 app.include_router(user)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=8000, log_level="info", reload=True)
+    
