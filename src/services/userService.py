4010fc28882d12dd10bf2c4120274f1e8abad4ce
Josue Luque <jluqueherbas@gmail.com>
2024-01-05 12:48:13 -0300

    Added in user service update and delete

--- src/services/userService.py
+++ src/services/userService.py
@@ -3,18 +3,39 @@ from src.models.userModel import users
 from cryptography.fernet import Fernet
 
 
+
 key = Fernet.generate_key()
 f = Fernet(key)
 
 def all_users():
-    return conn.execute(users.select()).fetchall()
+    query = users.select()
+    return conn.execute(query).fetchall()
 
-def new_user(user):
+def add_new_user(user):
     new_user = {"name": user.name, "email": user.email, "estado": user.estado}
     new_user["password"] = f.encrypt(user.password.encode("utf-8"))
 
-    result = conn.execute(users.insert().values(new_user))
-    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+    query = users.insert().values(new_user)
+    result = conn.execute(query)
+
+    query_rerturn = users.select().where(users.c.id == result.lastrowid)
+    return conn.execute(query_rerturn).first()
 
 def find_by_id(id):
-    return conn.execute(users.select().where(users.c.id == id)).first()
+    query = users.select().where(users.c.id == id)
+    return conn.execute(query).first()
+
+def update_by_id(id, user):
+    query = users.update().values(name = user.name, email = user.email, password = f.encrypt(user.password.encode("utf-8"))).where(users.c.id == id)
+    conn.execute(query)
+
+    query_return = users.select().where(users.c.id == id)
+    return conn.execute(query_return).first()
+
+
+def delete_by_id(id):
+    #result = conn.execute(users.select().where(users.c.id == id)).first
+
+    query = users.delete().where(users.c.id == id)
+    return conn.execute(query)
+
