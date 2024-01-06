72eb08615f058e994905c9f84703c3202c7322e1
Josue Luque <jluqueherbas@gmail.com>
2024-01-06 15:19:19 -0300

    Added Exceptions in user route

--- src/routes/userRoute.py
+++ src/routes/userRoute.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status, HTTPException
 from src.schemas.userSchema import User
 from src.services import userService
-from starlette.status import HTTP_204_NO_CONTENT
+from typing import Union
 
 
 user = APIRouter()
@@ -11,34 +11,47 @@ user = APIRouter()
 
 @user.get("/user/users", response_model=list[User], tags=["user"])
 def get_users():
-    return userService.all_users()
-
+    try:
+        return userService.all_users()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to get all users. ERROR: {str(e)}")
+    
 @user.post("/user/createUser", response_model=User, tags=["user"])
 def create_user(user: User):
-    return userService.add_new_user(user)
+    try:
+        return userService.add_new_user(user)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to create user. ERROR: {str(e)}")
 
-@user.get("/user/getUser/{userId}", tags=["user"])
+
+@user.get("/user/getUser/{userId}", response_model=Union[User, str], tags=["user"])
 def get_user(id: str):
-    result = userService.find_by_id(id)
-    if (result):
-        return result
-    else: 
-        return f"No se encontro el usuario con id: {id}"
-
-@user.put("/user/editUser/{userId}", response_model=User, tags=["user"])
-def update_user(id: str,user: User):
-    exis_user = userService.find_by_id(id)
-    if (exis_user):
-        return userService.update_by_id(id,user)
-    else:
-        return "Failed to update user"
+    existing_user = userService.find_by_id(id)
+    if existing_user:
+        return existing_user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {id}")
 
 
-@user.delete("/user/removeUser/{userId}", tags=["user"])
+@user.put("/user/editUser/{userId}", response_model=Union[User, str], tags=["user"])
+def update_user(id: str, user: User):
+    existing_user = userService.find_by_id(id)
+    if (existing_user):
+        try:
+            return userService.update_by_id(id, user)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to update user, ID: {id}. ERROR: {str(e)}")
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {id}")
+
+
+@user.delete("/user/removeUser/{userId}", response_model=str, tags=["user"])
 def delete_user(id: str):
-    exis_user = userService.find_by_id(id)
-    if (exis_user):
-        userService.delete_by_id(id)
-        return "Successfully deleted"
-    else:
-        return "Failed to delete user"
+    existing_user = userService.find_by_id(id)
+    if (existing_user):
+        try:
+            userService.delete_by_id(id)
+            return f"Successfully deleted, user ID: {id}"
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to delete user, ID: {id}. ERROR: {str(e)}")
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {id}")
