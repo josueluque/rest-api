81274487510e26403e172e8867243ff3591a472a
Josue Luque <jluqueherbas@gmail.com>
2024-01-09 16:51:11 -0300

    Added exceptions and validations

--- src/routes/userRoute.py
+++ src/routes/userRoute.py
@@ -6,8 +6,6 @@ from typing import Union
 
 user = APIRouter()
 
-#TODO Agregar Exceptions y Status code
-#TODO Seperar funciones en user service y agregar docstrings
 
 @user.get("/user/users", response_model=list[User], tags=["user"])
 def get_users():
@@ -16,8 +14,13 @@ def get_users():
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to get all users. ERROR: {str(e)}")
     
+
 @user.post("/user/createUser", response_model=User, tags=["user"])
 def create_user(user: User):
+    existing_user = userService.find_by_user_name(user.name)
+    if (existing_user):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User could not be created. Existing user: {user.name}")
+
     try:
         return userService.add_new_user(user)
     except Exception as e:
@@ -26,32 +29,38 @@ def create_user(user: User):
 
 @user.get("/user/getUser/{userId}", response_model=Union[User, str], tags=["user"])
 def get_user(userId: str):
-    existing_user = userService.find_by_id(userId)
-    if existing_user:
-        return existing_user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
+    existing_user = userService.find_by_user_id(userId)
+    if (not existing_user):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
+
+    return existing_user
 
 
 @user.put("/user/editUser/{userId}", response_model=Union[User, str], tags=["user"])
 def update_user(userId: str, user: User):
-    existing_user = userService.find_by_id(userId)
-    if (existing_user):
-        try:
-            return userService.update_by_id(userId, user)
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to update user, ID: {userId}. ERROR: {str(e)}")
+    existing_user = userService.find_by_user_id(userId)
+    if (not existing_user):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
+    
+    existing_name = userService.find_by_user_name(user.name)
+    if (existing_name):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User could not be created. Existing user: {user.name}")
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
+    try:
+        return userService.update_by_id(userId, user)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to update user, ID: {userId}. ERROR: {str(e)}")
 
 
 @user.delete("/user/removeUser/{userId}", response_model=str, tags=["user"])
 def delete_user(userId: str):
-    existing_user = userService.find_by_id(userId)
-    if (existing_user):
-        try:
-            userService.delete_by_id(userId)
-            return f"Successfully deleted, user ID: {userId}"
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to delete user, ID: {userId}. ERROR: {str(e)}")
+    existing_user = userService.find_by_user_id(userId)
+    if (not existing_user):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
+    
+    try:    
+        userService.delete_by_id(userId)
+        return f"Successfully deleted, user ID: {userId}"
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to delete user, ID: {userId}. ERROR: {str(e)}")
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"User not found, ID: {userId}")
--- src/services/userService.py
+++ src/services/userService.py
@@ -1,7 +1,7 @@
 from src.db.db import conn
 from src.models.userModel import users
 from cryptography.fernet import Fernet
-
+import re
 
 key = Fernet.generate_key()
 f = Fernet(key)
@@ -10,26 +10,43 @@ def all_users():
     query = users.select()
     return conn.execute(query).fetchall()
 
+
+def find_by_user_id(id):
+    query = users.select().where(users.c.id == id)
+    return conn.execute(query).first()
+
+
+def find_by_user_name(name):
+    query = users.select().where(users.c.name == name)
+    return conn.execute(query).first()
+
+
+def validate_email(email):
+    if not isinstance(email, str) or not re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$", email):
+        return False
+    return True    
+
 def add_new_user(user):
-    new_user = {"name": user.name, "email": user.email, "state": user.state}
-    new_user["password"] = f.encrypt(user.password.encode("utf-8"))
+    valid_email = validate_email(user.email)
+    if (not valid_email):
+        raise TypeError("Invalid email format")
+
+    encrypt_password = f.encrypt(user.password.encode("utf-8"))
+    new_user = {"name": user.name, "email": user.email, "password": encrypt_password, "state": user.state}
 
     query = users.insert().values(new_user)
     result = conn.execute(query)
 
-    query_return = users.select().where(users.c.id == result.lastrowid)
-    return conn.execute(query_return).first()   
+    return find_by_user_id(id = result.lastrowid)
 
-def find_by_id(id):
-    query = users.select().where(users.c.id == id)
-    return conn.execute(query).first()
+
 
 def update_by_id(id, user):
-    query = users.update().values(name = user.name, email = user.email, password = f.encrypt(user.password.encode("utf-8"))).where(users.c.id == id)
+    encrypt_password  = f.encrypt(user.password.encode("utf-8"))
+    query = users.update().values(name = user.name, email = user.email, password = encrypt_password).where(users.c.id == id)
     conn.execute(query)
 
-    query_return = users.select().where(users.c.id == id)
-    return conn.execute(query_return).first()
+    return find_by_user_id(id)
 
 
 def delete_by_id(id):
