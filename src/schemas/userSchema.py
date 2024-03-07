2050e4ca31c1927af6cecd89aa7a517664640c6c
Josue Luque <jluqueherbas@gmail.com>
2024-03-07 18:37:49 -0300

    Field validations

--- src/schemas/userSchema.py
+++ src/schemas/userSchema.py
@@ -1,13 +1,30 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class User(BaseModel):
     id: Optional[int] = None
-    name: str
+    """
+    - Validaciones con Field - 
+    -> max_length = cantidad maxima de caracteres (int)
+    -> min_lenght = cantidad minima de caracteres (int)
+    -> default = mensaje que aparecera en el campo a completar (str)
+
+    name: str = Field(default="My name", min_length=2, max_length=15) 
+    """
+    name: str = Field(max_length=15)    
     email: str
     password: str
-    state: bool
+    state: Optional[bool] = False
 
-    # class Config:
-    #     orm_mode = True
+    class Config:
+        # orm_mode = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Example Name",
+                "email": "example@gmail.com",
+                "password": "Secure Password",
+                "state": False,
+            }
+        }
 
