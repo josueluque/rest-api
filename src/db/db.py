d4ff6876e52b8ab40a4b58b488ac1df43a314a59
Josue Luque <jluqueherbas@gmail.com>
2024-03-08 14:52:00 -0300

    Environment variables for DATABASE_URL and preparing SECURE_KEY

--- src/db/db.py
+++ src/db/db.py
@@ -1,8 +1,9 @@
 # Importación del módulo necesario para la conexión con MySQL
 from sqlalchemy import create_engine, MetaData
+from decouple import config
 
 # Establecimiento de la conexión con MySQL
-engine = create_engine("mysql+pymysql://root:mysql@localhost:3306/restapidb")
+engine = create_engine(config('DATABASE_URL'))
 
 meta = MetaData() # Es un objeto de sqlalchemy.MetaData que se utiliza para almacenar información sobre la estructura de la base de datos, como las tablas y sus columnas.
 
