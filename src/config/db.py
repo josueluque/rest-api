fcb3c19e821ea10d92051fc98d8ead498b72db71
Josue Luque <jluqueherbas@gmail.com>
2024-04-08 19:22:06 -0300

    changed the database connection to use get_session

--- src/config/db.py
+++ src/config/db.py
@@ -1,10 +1,36 @@
-# Importación del módulo necesario para la conexión con MySQL
-from sqlalchemy import create_engine, MetaData
-from src.config.settings import get_settings
+# # Importación del módulo necesario para la conexión con MySQL
+# from sqlalchemy import create_engine, MetaData
+# from src.config.settings import get_settings
 
-# Establecimiento de la conexión con MySQL
+# # Establecimiento de la conexión con MySQL
+# settings = get_settings()
+
+# engine = create_engine(settings.DATABASE_URI)
+# meta = MetaData()
+# conn = engine.connect().execution_options(isolation_level="AUTOCOMMIT")
+
+'''new db connection'''
+
+from src.config.settings import get_settings
+from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import Generator
+from sqlalchemy import create_engine
+
 settings = get_settings()
 
-engine = create_engine(settings.DATABASE_URI)
-meta = MetaData()
-conn = engine.connect().execution_options(isolation_level="AUTOCOMMIT")
+engine = create_engine(settings.DATABASE_URI,
+                       pool_pre_ping=True,
+                       pool_recycle=3600,
+                       pool_size=20,
+                       max_overflow=0)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=True, autoflush=False)
+Base = declarative_base()
+
+
+def get_session() -> Generator:
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
